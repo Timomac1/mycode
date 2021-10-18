bfdf4a291b2d89e94ab167eca8e2ebf5cce18000
timo.mac1@gmail.com <student@bchd.964d9594-2bf4-42c3-b9fe-ffbee68d6736>
2021-10-18 18:58:28 +0000

    mixe lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#Create a list of different items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#Return the first item
+print("The first item in the list (IP): " + my_list[0] )
+
+#REturn item 2
+print("The second item in the list (port): " + str(my_list[1]) )
+
+#return item 3
+print("The last item in the list (state): " + my_list[2] )
+
